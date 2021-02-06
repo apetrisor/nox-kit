9fe08306da59068841d85393400274cca9958887
Alex Petrisor <alexgpetrisor@gmail.com>
2021-02-06 16:21:34 +0000

    Initial commit

---
+++ rollup.config.js
@@ -0,0 +1,124 @@
+import resolve from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
+import commonjs from '@rollup/plugin-commonjs';
+import svelte from 'rollup-plugin-svelte';
+import babel from '@rollup/plugin-babel';
+import {terser} from 'rollup-plugin-terser';
+import config from 'sapper/config/rollup.js';
+import pkg from './package.json';
+import alias from '@rollup/plugin-alias';
+import path from 'path';
+import sveltePreprocess from 'svelte-preprocess';
+
+const mode = process.env.NODE_ENV;
+const dev = mode === 'development';
+const legacy = !!process.env.SAPPER_LEGACY_BUILD;
+
+const onwarn = (warning, onwarn) =>
+	(warning.code === 'MISSING_EXPORT' && /'preload'/.test(warning.message)) ||
+	(warning.code === 'CIRCULAR_DEPENDENCY' && /[/\\]@sapper[/\\]/.test(warning.message)) ||
+	(warning.code === 'PLUGIN_WARNING' && warning.pluginCode === 'a11y-no-onchange') ||
+	onwarn(warning);
+
+const preprocess = sveltePreprocess({
+	scss: {
+		includePaths: ['src'],
+	},
+	postcss: true,
+	preserve: ['ld+json'],
+});
+
+export default {
+	client: {
+		input: config.client.input(),
+		output: config.client.output(),
+		watch: {chokidar: true, exclude: 'node_modules/**'},
+		plugins: [
+			alias({
+				resolve: ['.js', '.svelte'], // optional, by default this will just look for .js files or folders
+				entries: [{find: '@', replacement: path.resolve(__dirname, 'src')}],
+			}),
+			replace({
+				'process.browser': true,
+				'process.env.NODE_ENV': JSON.stringify(mode),
+			}),
+			svelte({
+				preprocess,
+				compilerOptions: {
+					dev,
+					hydratable: true,
+				},
+			}),
+			resolve({
+				browser: true,
+				dedupe: ['svelte'],
+			}),
+			commonjs(),
+
+			legacy &&
+				babel({
+					extensions: ['.js', '.mjs', '.html', '.svelte'],
+					babelHelpers: 'runtime',
+					exclude: ['node_modules/@babel/**'],
+					presets: [
+						[
+							'@babel/preset-env',
+							{
+								targets: '> 0.25%, not dead',
+							},
+						],
+					],
+					plugins: [
+						'@babel/plugin-syntax-dynamic-import',
+						[
+							'@babel/plugin-transform-runtime',
+							{
+								useESModules: true,
+							},
+						],
+					],
+				}),
+
+			!dev &&
+				terser({
+					module: true,
+				}),
+		],
+
+		preserveEntrySignatures: false,
+		onwarn,
+	},
+
+	server: {
+		input: config.server.input(),
+		output: config.server.output(),
+		watch: {chokidar: true, exclude: 'node_modules/**'},
+		plugins: [
+			alias({
+				resolve: ['.js', '.svelte'], // optional, by default this will just look for .js files or folders
+				entries: [{find: '@', replacement: path.resolve(__dirname, 'src')}],
+			}),
+			replace({
+				'process.browser': false,
+				'process.env.NODE_ENV': JSON.stringify(mode),
+			}),
+			svelte({
+				preprocess,
+				compilerOptions: {
+					dev,
+					generate: 'ssr',
+					hydratable: true,
+				},
+				emitCss: false,
+			}),
+			resolve({
+				dedupe: ['svelte'],
+			}),
+			commonjs(),
+		],
+		external: Object.keys(pkg.dependencies).concat(require('module').builtinModules),
+
+		preserveEntrySignatures: 'strict',
+		onwarn,
+	},
+};
