e0555c71407127eff6017640749ccba0b1f50287
Alex Petrisor <alexgpetrisor@gmail.com>
2021-03-22 18:19:37 +0000

    CLI

--- cli.js
+++ cli.js
@@ -1,3 +1,13 @@
-#!/usr/bin/env node
+#!/usr/bin/env bash
 
-console.log('Hello!');
+if [ "$1" = "dev" ]; then
+	echo "Starting dev environment..."
+	./node_modules/.bin/postcss src/assets/global.pcss -o static/css/global.css -w --config ./node_modules/@apetrisor/nox-kit &
+	env $(cat .env | xargs) ./node_modules/.bin/sapper dev
+elif [ "$1" = "build" ]; then
+	echo "Starting build..."
+	NODE_ENV=production ./node_modules/.bin/postcss src/assets/global.pcss -o static/css/global.css --config ./node_modules/@apetrisor/nox-kit
+	env $(cat .env | xargs) ./node_modules/.bin/sapper build --legacy
+elif [ "$1" = "start" ]; then
+	env $(cat .env | xargs) node __sapper__/build
+fi
