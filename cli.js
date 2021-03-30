ce8c97fe5bcce410484677cc94a7134e9ba1da16
Alex Petrisor <alexgpetrisor@gmail.com>
2021-03-30 13:34:19 +0100

    Replaced __sapper__ with nox, no more nox start

--- cli.js
+++ cli.js
@@ -3,11 +3,9 @@
 if [ "$1" = "dev" ]; then
 	echo "Starting dev environment..."
 	env $(cat .env | xargs) ./node_modules/.bin/postcss ./src/assets/global.pcss -o ./static/css/global.css --config ./node_modules/@apetrisor/nox-kit
-	env $(cat .env | xargs) ./node_modules/.bin/sapper dev
+	env $(cat .env | xargs) ./node_modules/.bin/sapper dev --build-dir ./nox/dev
 elif [ "$1" = "build" ]; then
 	echo "Starting build..."
 	env $(cat .env | xargs) NODE_ENV=production ./node_modules/.bin/postcss src/assets/global.pcss -o static/css/global.css --config ./node_modules/@apetrisor/nox-kit
-	env $(cat .env | xargs) NODE_ENV=production ./node_modules/.bin/sapper build --legacy
-elif [ "$1" = "start" ]; then
-	env $(cat .env | xargs) node __sapper__/build
+	env $(cat .env | xargs) NODE_ENV=production ./node_modules/.bin/sapper build ./nox/build --legacy
 fi
